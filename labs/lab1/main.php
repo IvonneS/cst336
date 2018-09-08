9fa6b87960aa0e9db3bb78bbbb98118d946b0643
Ivonne Garcia Santos <ivonnes118@gmail.com>
2018-09-08 13:13:49 +0000

    adding foldes

---
+++ labs/lab1/main.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=”utf-8” /> <!--includes most characters from languages-->
+        <title> My New Website </title> <!--Title on browser-->
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+       <style>
+           header, nav, footer, figure{
+           text-align: center;
+       }
+       </style>
+       
+    </head>
+        <body>
+            <header>
+            
+            <h1> *WELCOME!* </h1>
+            
+            </header>
+        
+         <nav>
+            
+             <hr width="100%"/>
+             <strong>
+             <a href= "main.php">HOME</a>
+             
+             <a href= "about.php">ABOUT</a>
+             
+             <a href= "contact.php">CONTACT</a>
+             </strong>
+         </nav>
+        
+         
+         <br /><br />
+                
+                
+                     
+                 <main>
+                    <div>
+                       <figure>
+                           <img src="img/me.jpg" width="400" height="500"/>
+                     </figure>
+                     </div>
+               <div id="paragraph">
+                     <p style="text-align:left">
+                       Hello! <br>
+                       My name is Ivonne Garcia and <br> 
+                       I am transfer stundent in CSU Monterey Bay.<br>
+                       I am study to be a Software Engineer. </p>
+                       </br>
+                       <p>
+                       <em>"Yesterday is gone. Tomorrow has not yet come. We have only today. Let us begin" </em><br />
+                  - Mother Teresa
+                    </p>
+                    </div> 
+               
+                
+
+         </main>
+         
+         
+         
+         <hr width="100%"/>
+         
+         <footer>
+              <em> CST336 Internet Programming. 2018&copy; Garcia<br /></em>
+             <strong>Disclaimer:</strong> The information in this webpage fictitous. <br />
+             It is used for academic purposes only.<br>
+             <img src="img/csumb.png" width="100" height="50"/>
+         </footer>
+         
+    </body>
+</html>
