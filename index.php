ea8b8aad8292231c161d4c0b77b2698c56dfc43e
Ivonne Garcia Santos <ivonnes118@gmail.com>
2018-09-08 13:25:32 +0000

    adding foldes

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<html><body>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ivonne's </title>
+    </head>
+    <body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
+echo "Ivonne's cst336 web site";
 
 ?>
 </body>
