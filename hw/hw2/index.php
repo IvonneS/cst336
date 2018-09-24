9b06f11ea3ffa91f646263e53527c3ad377c2a60
Ivonne Garcia Santos <ivonnes118@gmail.com>
2018-09-24 18:20:06 +0000

    hw2

---
+++ hw/hw2/index.php
@@ -0,0 +1,45 @@
+<?php
+        include 'functions.php';
+    ?>
+<!DOCTYPE html>
+<html>
+    <head>
+         <style>
+            @import url("css/style.css");
+        </style>
+        <title>Hw2</title>
+        <h1>Album Generator</h1>
+       
+    </head>
+    <body>
+        <div class= "row">
+             <div class = "column">
+                 <img src="img/hand.jpg" alt = "hand" width = "75px"/>
+             </div>
+             <div class = "column">
+                 <img src="img/rock/acdc.jpg" alt = "2nd image" width = "85px"/>
+             </div>
+             <div class = "column">
+                 <img src="img/gorra.jpg" alt = "gorra" width = "75px"/>
+             </div>
+             <div class = "column">
+                 <img src="img/cowboy/jaguar.jpg" alt = "2nd image" width = "85px"/>
+             </div>  
+        </div>
+         <div class= "row">
+             <div class = "column">
+                 <img src="img/jazz.jpg" alt = "jazz" width = "75px"/>
+             </div>
+             <div class = "column">
+                 <img src="img/jazz/pic1.jpg" alt = "2nd image" width = "90px"/>
+             </div>
+             <div class = "column">
+                 <img src="img/pop.jpg" alt = "pop" width = "75px"/>
+             </div>
+             <div class = "column">
+                 <img src="img/pop/p1.jpg" alt = "2nd image" width = "90px"/>
+             </div>  
+        </div>
+
+    </body>
+</html>
