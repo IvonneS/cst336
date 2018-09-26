e0062a70004d05c29f36ab117440101c844cae81
Ivonne Garcia Santos <ivonnes118@gmail.com>
2018-09-26 04:17:49 +0000

    reedoing index file

---
+++ hw/hw2/index.php
@@ -0,0 +1,39 @@
+<?php
+        include 'functions.php';
+    ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            @import url("css/style.css");
+        </style>
+        <title>Homework2 </title>
+    </head>
+    <body>
+        <h2>Albumm Generator</h2>
+      <div class ="row">
+             <div class = "column">
+                  <form>
+            <input type="submit" name=""/>
+        </form>
+                 <img src="img/hand.jpg" alt = "1st image" width = "75px"/>
+                  
+             </div>
+             <div class = "column">
+                 <img src="img/gorra.jpg" alt = "2nd image" width = "75px"/>
+                 
+             </div>
+      <div class ="row">
+             <div class = "column">
+                 <img src="img/jazz.jpg" alt = "1st image" width = "75px"/>
+                  
+             </div>
+             <div class = "column">
+                 <img src="img/pop.jpg" alt = "1st image" width = "75px"/>
+             </div>
+       
+        </div>
+    </body>
+       <footer>
+       </footer>
+</html>
