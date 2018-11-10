da3dbafb4775981337dfff3ce078aeb15499e889
Ivonne Garcia Santos <ivonnes118@gmail.com>
2018-11-10 18:35:57 +0000

    sfsf

--- labs/lab7/index.php
+++ labs/lab7/index.php
@@ -2,14 +2,15 @@
 <html>
     <head>
         <title> Admin Login </title>
+         <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
 
         <h1> Ottermart - Admin Login </h1>
         
         <form method="post" action="loginProcess.php">
-          Username:  <input type="text" name="username"/> <br>
-          Password:  <input type="password" name="password"/> <br>
+          Username:  <input type="text" name="username"/> <br><br>
+          Password:  <input type="password" name="password"/> <br><br>
           <input type="submit" value="Login">
         </form>
 
